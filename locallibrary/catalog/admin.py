1e3a30b932599e574563f82776cd62114bdc031b
Woobean Seo <seowoobean@gmail.com>
2020-05-30 01:54:39 +0900

    part3-challenge (add Language Model)

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
@@ -7,10 +7,10 @@ class AuthorAdmin(admin.ModelAdmin):
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
-    
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author','display_genre')
+    list_display = ('title', 'author','language','display_genre')
     inlines = [BooksInstanceInline]
     
 @admin.register(BookInstance) 
@@ -27,4 +27,5 @@ class BookInstanceAdmin(admin.ModelAdmin):
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
+admin.site.register(Language)
 
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -13,7 +13,7 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     def __str__(self):
         return self.title
@@ -68,3 +68,10 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
+class Language(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        return self.name
